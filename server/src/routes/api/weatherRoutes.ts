9b307c4cd080191ae9a314616a5fa825a8e3348e
Justin Miller <justinmiller617@gmail.com>
2024-11-21 16:50:12 -0600

    updated code in api folder

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -6,19 +6,44 @@ import WeatherService from '../../service/weatherService.js';
 
 // TODO: POST Request with city name to retrieve weather data
 router.post('/', async (req: Request, res: Response) => {
-  const {cityName} = req.body;
-  try {
-    await HistoryService.addCity(cityName);
-    res.status(201).send({message: `City ${cityName} added to history`})
+  const { cityName } = req.body;
+
+  if (!cityName) {
+    return res.status(400).json({ message: 'City name is required' });
   }
-  // TODO: GET weather data from city name
-  // TODO: save city to search history
+
+  try {
+    const weatherData = await WeatherService.getWeatherForCity(cityName);
+    await HistoryService.addCity(cityName);
+    return res.status(200).json({
+      message: 'Weather data retrieved and city saved to history',
+      weatherData,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Failed to fetch weather data or save city' });
+  }
 });
 
 // TODO: GET search history
-router.get('/history', async (req: Request, res: Response) => {});
+router.get('/history', async (_req: Request, res: Response) => {
+  try {
+    const cities = await HistoryService.getCities();
+    res.status(200).json(cities);
+  } catch (error) {
+    res.status(500).send({message: `Error fetching search history`, error})
+  }
+});
 
 // * BONUS TODO: DELETE city from search history
-router.delete('/history/:id', async (req: Request, res: Response) => {});
+router.delete('/history/:id', async (req: Request, res: Response) => {
+  const {id} = req.params;
+  try {
+    await HistoryService.removeCity(id);
+    res.status(200).send({message: `City with ID ${id} removed from history`});
+  } catch (error) {
+    res.status(500).send({message: `Error removing city from history`, error})
+  }
+});
 
 export default router;
